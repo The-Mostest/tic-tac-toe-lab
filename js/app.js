c1092cdec8e41ce6b287dc7b3b467a93e1b7959e
the-mostest <fbmcdougall@gmail.com>
2024-09-12 10:58:35 +0100

    Correct Code

--- js/app.js
+++ js/app.js
@@ -32,40 +32,101 @@ const squareEls = document.querySelectorAll('.sqr');
 
 
 
-const updateBoard = () => {
+const updateBoard = function () {
     board.forEach((cell, idx) => {
-        if (cell === 'X') {
-            squareEls[idx].textContent = 'X'}
-            else if (cell === 'O') {
-                squareEls[idx].textContent = 'O'}
-                else {
-                    squareEls[idx].textContent = ''
-                }
-            }
-    )}
+      if (cell === "X") {
+        squareEls[idx].textContent = "X";
+      } else if (cell === "O") {
+        squareEls[idx].textContent = "O";
+      } else {
+        squareEls[idx].textContent = "";
+      }
+    });
+  };
 
-    const updateMessage = () => {
-        if (!winner  && !tie) {
-            if (turn === 'X')
-                messageEl.textContent = 'Xs turn'
-        } else if (!winner  && !tie) {
-            if (turn === 'O')
-                messageEl.textContent = 'Os turn'
-    }   else if (!winner && tie) {
-            messageEl.textContent = "It's a Tie!"
+  const updateMessage = () => {
+    if (!winner && !tie) {
+      if (turn === 'X') {
+        messageEl.textContent = "It's X's turn"
+      } else {
+        messageEl.textContent = "It's O's turn"
+      }
+    } else if (!winner & tie) {
+      messageEl.textContent = "It's a tie"
     } else {
-        if (turn === 'X') {
-            messageEl.textContent ='X won!'
-        } else {
-            messageEl.textContent = 'O won!'
-        }
+      if (turn === 'X') {
+        messageEl.textContent = "X wins"
+      } else {
+        messageEl.textContent = "O wins"
+      }
     }
-    }
+  }
 
-    const render = () => {
+const render = () => {
         updateBoard();
         updateMessage();
     };
+
+
+
+
+
+
+const placePiece = (index) => {
+    board[index] = turn;
+}
+
+
+const checkForWinner = () => {
+    if  (
+        (board[0] !== '' && board[0] === board[1] && board[0] === board[2]) ||  
+        (board[3] !== '' && board[3] === board[4] && board[3] === board[5]) ||  
+        (board[6] !== '' && board[6] === board[7] && board[6] === board[8]) ||  
+        (board[0] !== '' && board[0] === board[3] && board[0] === board[6]) ||  
+        (board[1] !== '' && board[1] === board[4] && board[1] === board[7]) ||  
+        (board[0] !== '' && board[0] === board[4] && board[0] === board[8]) ||  
+        (board[2] !== '' && board[2] === board[5] && board[2] === board[8]) ||  
+        (board[2] !== '' && board[2] === board[4] && board[2] === board[6])     
+    ) { winner = true} ;
+}
+
+
+
+
+const checkForTie = () => {
+    if (winner) {
+        return;
+    }
+    if (!board.includes('')) {
+        tie = true;
+    }
+}
+
+
+const switchPlayerTurn = () => {
+    if (winner) {
+        return;
+    }if (turn === 'X') { 
+        turn = 'O'
+    } else {
+        turn = 'X'
+    }
+}
+
+const handleClick = (event) => {
+    const squareIndex = event.target.id;
+    const squareIsFull = board[squareIndex] !== ''
+       if (squareIsFull || winner) {
+           return
+       }
+           placePiece(squareIndex);
+           checkForWinner();
+           checkForTie();
+           switchPlayerTurn();
+           render();
+   }
+   
+
 
     
 function init() {
@@ -79,78 +140,6 @@ function init() {
     init();
 
 
-
-
-
-
-const handleClick = (event) => {
- const squareIndex = event.target.id;
-    if(winner === true || board[squareIndex] === '')
-        placePiece(squareIndex);
-        checkForWinner();
-        checkForTie();
-        console.log(tie)
-}
-
-
-
-
-const placePiece = (index) => {
-    board[index] = turn;
-    console.log(board)
-}
-
-
-const checkForWinner = () => {
-    if  (
-        ((board[0] !== '') && (board[0] === board[1]) && (board[0] === board[2])) ||  
-        ((board[3] !== '') && (board[3] === board[4]) && (board[3] === board[5])) ||  
-        ((board[6] !== '') && (board[6] === board[7]) && (board[6] === board[8])) ||  
-        ((board[0] !== '') && (board[0] === board[3]) && (board[0] === board[6])) ||  
-        ((board[1] !== '') && (board[1] === board[4]) && (board[1] === board[7])) ||  
-        ((board[0] !== '') && (board[0] === board[4]) && (board[0] === board[8])) ||  
-        ((board[2] !== '') && (board[2] === board[5]) && (board[2] === board[8])) ||  
-        ((board[2] !== '') && (board[2] === board[4]) && (board[2] === board[6]))     
-    ) { winner = true} ;
-}
-
-
-// const checkForWinner = () => {
-//    winningCombos.forEach ((combo) => {
-    
-//    console.log('winner')
-//     })};
-
-
-    
-// const checkForWinner = () => {
-//     winningCombos.forEach((combo) => {
-//         if(board.indexOf[0] === combo[0])
-//             console.log('wonner')
-//     })
-// };
-
-
-
-
-const checkForTie = () => {
-    if (winner === true )
-        return;
-     else if (!board === '') {
-        tie ===
-     }
-}
-
-
-const switchPlayerTurn = () => {
-    if (winner === true) {
-        return;
-    } else if ( winner === false && turn === 'X') { 
-        turn = 'O'
-    } else if (winner === false && turn === 'O') {
-        turn = 'X'
-    }
-}
 
 
 
